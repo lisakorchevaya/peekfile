1117df08ded424bf11a89221134c00ce4573419d
Elizaveta Korchevaya <147381838+lisakorchevaya@users.noreply.github.com>
2023-10-25 15:43:28 +0200

    Create peek.sh

---
+++ peek.sh
@@ -0,0 +1,12 @@
+#usr/bin/bash
+#define the arguments
+file = "$1"
+n_lines = "$2"
+# print the first 3 lines
+head -n "n_lines" "file"
+
+# print a separator line
+echo "..."
+
+# print the last 3 lines
+tail -n "n_lines" "file"
