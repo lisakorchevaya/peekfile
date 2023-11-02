cea59b2ddb3a3c6ba9342b260d1a47d53bfa537f
Elizaveta <lisa@Lisa-3.local>
2023-11-02 15:12:29 +0100

    peek update

--- peek.sh
+++ peek.sh
@@ -6,12 +6,10 @@ else
     lines=$2
 fi
 
-total_lines=$(wc -l < "$1")
-
-if [ "$total_lines" -lt $((2 * lines)) ]; then
+if [ $(wc -l < "$1") -lt $((2 * lines)) ]; then
     cat "$1"
 else
-    echo "The file has more than $((2 * lines)) lines. Displaying the first $lines and last $lines lines:"
+    echo "Warning: This file has more than twice the $lines lines. Providing only the first $lines and last $lines lines:"
     head -n "$lines" "$1"
     echo "..."
     tail -n "$lines" "$1"
