17f786744bf74f42e6c2f7f6ab7b334fc8e41c7f
Elizaveta <lisa@Lisa-3.local>
2023-11-19 21:56:12 +0100

    draft 2

--- fastascan.sh
+++ fastascan.sh
@@ -1,29 +1,53 @@
 # This script allows you to scan any directory for fasta/fa files
 # The resulting output is a brief report on the contents of fasta/fa
 # USAGE
-# ./fastascan.sh /path/to/directory
-if [[ -d "$1" ]]; then
+# ./fastascan.sh /path/to/folder number of lines (an integer)
+
+if [[ -n "$1" && -d "$1" ]]; then
   folder="$1"
+  echo "Working in $folder"
+  echo "..."
 else
-  echo "Warning: no path provided, working in the current directory."
   folder=$(pwd)
+  echo "Warning: no path provided, working in the current directory $folder."
+  echo "..."
 fi
 
+# Brief summary of the folder
+echo "Brief summary of the folder and its subfolders:"
 for file in $folder; do
   count_files=$(find . -name "*.fa" -or -name "*.fasta" | wc -l);
-  echo "There are a total of $count_files detected .fasta/.fa files";
+  echo "1. There are a total of $count_files detected .fasta/.fa files";
 
   unique_ids=$(grep ">" $(find . -name "*.fa" -or -name "*.fasta") | wc -l);
-  echo "There are a total of $unique_ids unique fasta IDs in all deteced files";
+  echo "2. There are a total of $unique_ids unique fasta IDs in all deteced files";
+  echo "..."
 done
 
+# Providing a summary for each file individually
+for file in $(find . -name "*.fa" -or -name "*.fasta");
+  do echo "Generating report for: $file";
+  if [[ -h "$file" ]]; then
+    echo "$file is a symlink"
+  else
+    echo "$file is a regular file"
+  fi
+  count_seq=$(grep ">" $file | wc -l);
+  echo "$file has $count_seq sequences"
+  echo "..."
 
-#lines=$2
+#  if [[ -z "$2" ]]; then
+#    lines=0
+#  else
+#    lines=$2
+#  fi
 
-# if [ $lines -le $((lines * 2)) ]; then
-#  cat file
-#else
-#  echo "Warning: This file has more than twice the $N_lines lines."
-#  head -n "$lines"
-#  echo "..."
-#  tail -n "$lines"
+#  if [[ $lines -le $((lines * 2)) ]]; then
+#    cat $file
+#  else
+#    echo "Warning: This file has more than twice the $lines lines. Showing the first and last $lines."
+#    head -n "$lines"
+#    echo "..."
+#    tail -n "$lines"
+#  fi
+done
